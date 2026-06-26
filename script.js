cd11e4f6346182335a38219274c07a08e105c254
sagithilyas27-sudo <sagithilyas27@gmail.com>
2026-06-26 12:27:44 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("HomePlan AI website loaded!");
+
+  const button = document.querySelector(".btn");
+
+  if (button) {
+    button.addEventListener("click", () => {
+      alert("AI House Planning feature coming soon!");
+    });
+  }
+});
